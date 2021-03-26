3b8185c5ffe03dec43638b41b5fdd379e5e20160
Drew <drewbarton10@gmail.com>
2021-03-26 15:58:08 -0600

    add box position

--- SudokuSolver/solvertext.py
+++ SudokuSolver/solvertext.py
@@ -7,7 +7,7 @@ Created on Tue Mar 16 17:38:10 2021
 The goal of this project is to use Deep Learning and the recursive backtracking algorithm to solve any sudoku puzzle.
 
 """
-#create grid with prefilled digits
+# create grid with prefilled digits
 board = [
         [7, 8, 0, 4, 0, 0, 1, 2, 0],
         [6, 0, 0, 0, 7, 5, 0, 0, 9],
@@ -22,21 +22,21 @@ board = [
 
 def valid(grid, num, pos):
 
-    #check row
+    # check row
     for i in range (len(grid[0])):
         if grid[pos[0]][i] == num and pos[1] != i:
             return False
 
-    #check column
+    # check column
     for i in range (len(grid[0])):
         if grid[i][pos[1]] == num and pos[0] != i:
             return False
 
-    #check position in box
+    # check position in box
     box_x = pos[1] // 3
     box_y = pos[0] // 3
 
-#draw grid function
+# draw grid function
 def print_board(grid):
     
     for i in range(len(grid)):
@@ -54,7 +54,7 @@ def print_board(grid):
 
 print_board(board)
 
-#loops board and finds null value and returns to call
+# loops board and finds null value and returns to call
 def find_empty(grid):
     for i in range(len(grid)):
         for j in range(len(grid[0])):
